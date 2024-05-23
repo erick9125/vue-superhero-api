660fa52f08500e3cb55045ea649dac0c09a42f7b
Erick Morales <44293951+erick9125@users.noreply.github.com>
2024-05-23 14:21:25 -0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createServer} from 'http'
 import { createProxyMiddleware} from 'http-proxy-middleware';
 
 // https://vitejs.dev/config/
